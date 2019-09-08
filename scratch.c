69e23b122e8418501173d9b04c7f58eeb08f8039
Richa Rathore <richa.rathore@students.iiit.ac.in>
2019-09-09 03:50:44 +0530

    With Environment variables and history added

--- scratch.c
+++ scratch.c
@@ -5,7 +5,48 @@
 #include <string.h>
 #include <assert.h>
 
+
 int piped=0;
+int single_out=0;
+int double_out=0;
+
+void initialization(){
+ FILE *fp;
+ fp=fopen("my_rc.txt","w+");
+ if(fp==NULL){
+   printf("file opening error\n");
+   return;
+ }
+ // fputs("PATH :");
+  char* strr = getenv("PATH");
+  fputs(strr,fp);
+  fputs("\n",fp);
+  char* strr2 = getenv("USER");
+  fputs(strr2,fp);
+  fputs("\n",fp);
+  char* strr4 = getenv("USERNAME");
+  fputs(strr4,fp);
+  fputs("\n",fp);
+  char* strr5 = getenv("HOME");
+  fputs(strr5,fp);
+  fputs("\n",fp);
+  fclose(fp);
+}
+
+void view_history(){
+   FILE* fp;
+   fp=fopen("history.txt","r");
+   if(fp==NULL){
+      printf("file opening error\n");
+      return;
+   }
+   char a[400];
+   while (fgets(a,sizeof(a),fp) != NULL)
+    {  
+       printf("%s\n",a);
+    }
+  }
+ 
 
 void runcmd(char** p){
     if(strcmp(p[0],"cd")==0) 
@@ -16,6 +57,73 @@ void runcmd(char** p){
     execvp(p[0],p);
     return;
 }
+
+void rc_search(char* a){
+  FILE* fp;
+  fp=fopen("my_rc.txt","r");
+  int count=0;
+  char data[128];
+  int i=0,j=1;
+  while(a[j]!='\0') data[i++]=a[j++];
+  data[i]='\0';
+  if(strcmp(data,"PATH")==0) 
+  {
+    while (fgets(data,sizeof(data),fp) != NULL)
+    {
+      if (count == 0)
+        {
+           printf("%s\n",data);
+           break;
+        }
+    }
+  }
+  else if(strcmp(data,"USER")==0) 
+  {
+    while (fgets(data,sizeof(data),fp) != NULL)
+    {
+      if (count == 1)
+        {
+           printf("%s\n",data);
+           break;
+        }
+        else
+        {
+            count++;
+        }
+    }
+  }
+  else if(strcmp(data,"USERNAME")==0) 
+  {
+    while (fgets(data,sizeof(data),fp) != NULL)
+    {
+      if (count == 2)
+        {
+           printf("%s\n",data);
+           break;
+        }
+        else
+        {
+            count++;
+        }
+    }
+  }
+  else{
+    while (fgets(data,sizeof(data),fp) != NULL)
+    {
+      if (count == 3)
+        {
+           printf("%s\n",data);
+           break;
+        }
+        else
+        {
+            count++;
+        }
+    }
+  }
+  fclose(fp);
+}
+
 
 
 char** get_tokens(char* input){
@@ -30,10 +138,48 @@ char** get_tokens(char* input){
        if(strcmp(temp,"|")==0){
          piped++;
        }
+       if(strcmp(temp,">")==0){
+         if(strcmp(tok[i-2],">")==0){
+             single_out--;
+             double_out++;
+         }
+         else single_out++;
+       }
        temp = strtok (NULL, " ");
     }
     return tok;
 }
+
+/*void runcmd_io(char** input){
+  int fd;
+  int pid;
+  int output;
+  pid = fork();
+//  output = open(input[1], O_WRONLY|O_CREAT|O_TRUNC, 0777);
+  /* if (output < 0)
+    {
+      printf("The opening of %s did not complete and has failed\n",input[1]);
+      exit(0);
+    }
+    if (dup2(output, fd) < 0) 
+    {
+      fprintf(stderr, "dup2 failed to execute\n");
+      exit(0);
+    }*/
+ /*  fd = creat(input[1], 0644);
+    dup2(fd, 1);
+    //out = 0;
+    output = dup(1);
+    if (pid == 0) {       
+      execvp(input[0], input);
+      perror("execvp");
+      _exit(1);
+} else {
+    waitpid(pid, 0, 0);
+    dup2(input[1], 1);
+    free(res);
+}
+}*/
 
 
 void runpipe_recursive(char ** input, int count, int fd[2])
@@ -114,46 +260,21 @@ void runpipe_recursive(char ** input, int count, int fd[2])
 void runcmd_pipe(char** input){
     int fd[2];
     runpipe_recursive(input, piped+1, fd);
-   /* pid_t pid;
-    int status;
-    if(pipe(fd)<0){
-         printf("pipe error\n");
-         return;
-       }
-     pid = fork();
-     if(pid<0)
-     {
-      printf("fork error\n");
-      exit(0);
-     }
-     else if (pid == 0)    
-     {
-      /*close(fd[0]);
-      dup2(fd[1], 1); 
-      runcmd(input);                         
-      close(fd[1]);*/
-    /*  close(fd[0]);
-      dup2(fd[1], 1);
-      close(fd[1]);
-      char** tok1;
-      tok1=get_tokens(input[0]);
-      execvp(tok1[0],tok1);
-    } 
-    else                                          
-    {
-      /*close(fd[1]);
-      dup2(fd[0], 0);
-      runcmd(p);
-      close(fd[0]);*/
-   /*   close(fd[1]);
-      dup2(fd[0], 0);
-      close(fd[0]);
-      char** tok;
-      tok=get_tokens(input[1]);
-      execvp(tok[0],tok);
-    }
-    wait(&status);*/
 }
+
+char** get_redirectok(char* input){
+    char* temp;
+    char** tok;
+    tok = (char**)malloc(64*sizeof(char*));
+    int i=0;
+    temp = strtok (input,">");
+    while(temp!=NULL){
+      tok[i++]=temp;
+       temp = strtok (NULL, ">");
+    }
+    return tok;
+}
+
 
 char** get_pipetok(char* input){
     char* temp;
@@ -169,8 +290,13 @@ char** get_pipetok(char* input){
 }
 
 
-
 char* read_input(void){
+    FILE* fp;
+    fp=fopen("history.txt","a");
+    if(fp==NULL){
+       printf("file opening error\n");
+       return NULL;
+    }
     int buffer = 2048;
     char* in = (char*)malloc(buffer*sizeof(char));
     if(!in) fprintf(stderr,"memory allocation problem\n");
@@ -185,10 +311,15 @@ char* read_input(void){
           in[i++]=c;
     }
      in[i]='\0';
+     fputs(in,fp);
+     fputs("\n",fp);
+     fclose(fp);
      return in;
 }
 
+
 int main(){
+  initialization();
   static char* input;
   char* input_copy = (char*)malloc(2048*sizeof(char));
   int fd, r; 
@@ -196,9 +327,13 @@ int main(){
     while(1){
     printf("\n$:");
     input = read_input();
+    if(input[0]=='$') rc_search(input);
+    else if(strcmp(input,"history")==0) view_history();
+    else{
     strcpy(input_copy,input);
     char** p;
     p = get_tokens(input);
+   // if(strcmp(p[0],"alias")==0) run_alias(p);
     if(strcmp(p[0],"exit")==0){
       exit(0);
       return 0;
@@ -207,15 +342,15 @@ int main(){
     if(pid < 0) printf("error in fork\n");
     if(pid==0){
         if(piped==0) runcmd(p);
-        else {
+        else{
              char** piped_inp = (char**)malloc(sizeof(char*));
               piped_inp = get_pipetok(input_copy);
              runcmd_pipe(piped_inp);
         }
     }
-   // printf("%d\n",piped);
     wait(&r);
     piped=0;
+   }
 }
   exit(0);
   return 0;
